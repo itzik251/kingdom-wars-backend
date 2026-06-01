136875a590068d7172241adebfb48750955b1f29
Itzik <itzik252@gmail.com>
2026-06-02 01:50:24 +0300

    feat: serve React frontend from NestJS static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AuthModule } from './modules/auth/auth.module';
 import { KingdomModule } from './modules/kingdom/kingdom.module';
@@ -48,6 +50,11 @@ import { TelegramModule } from './modules/telegram/telegram.module';
     }),
 
     ScheduleModule.forRoot(),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      exclude: ['/api/(.*)'],
+    }),
 
     AuthModule,
     KingdomModule,
